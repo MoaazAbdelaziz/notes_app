cc2e22b6e09a221fd85c86b411c89464ab612e91
MoaazAbdelaziz <moaazwork1@gmail.com>
2023-07-31 23:17:46 +0300

    color list UI and logic complete

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,29 +1,67 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isSelected, required this.color});
+  final bool isSelected;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 32,
-      backgroundColor: Colors.blue,
-    );
+    return isSelected
+        ? CircleAvatar(
+            radius: 35,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 32,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 32,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+
+  List<Color> colors = const [
+    Color(0xFFFA697C),
+    Color(0xFFE13A9D),
+    Color(0xFF9B45E4),
+    Color(0xFFFCC169),
+    Color(0xFF7A9D54),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 32 * 2,
+      height: 35 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (BuildContext context, int index) {
-          return const ColorItem();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: GestureDetector(
+              child: ColorItem(
+                color: colors[index],
+                isSelected: currentIndex == index,
+              ),
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+            ),
+          );
         },
       ),
     );
