8962a801ccb91a5fc8af7a2a9072f098bac5d97f
MoaazAbdelaziz <moaazwork1@gmail.com>
2023-07-31 21:57:16 +0300

    create colors list view

---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(
+      radius: 32,
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class ColorsListView extends StatelessWidget {
+  const ColorsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 32 * 2,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return const ColorItem();
+        },
+      ),
+    );
+  }
+}
